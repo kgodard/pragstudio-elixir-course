37d2a8d890f4155423f6cd93fc910e1ae38294fd
Kim Godard <kim.godard@gmail.com>
2019-01-06 18:21:26 +0000

    ch 8

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -2,9 +2,32 @@ defmodule Servy.Handler do
   def handle(request) do
     request
     |> parse
+    |> rewrite_path
+    |> log
     |> route
+    |> emojify
+    |> track
     |> format_response
   end
+
+  def track(%{status: 404, path: path} = conv) do
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(%{path: "/bears?id=" <> id} = conv) do
+    %{ conv | path: "/bears/#{id}" }
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def log(conv), do: IO.inspect conv
 
   def parse(request) do
     [method, path, _] = 
@@ -12,28 +35,140 @@ defmodule Servy.Handler do
       |> String.split("\n")
       |> List.first
       |> String.split(" ")
-    %{ method: method, path: path, resp_body: "" }
+
+    %{ method: method, 
+       path: path,
+       resp_body: "",
+       status: nil
+     }
   end
 
-  def route(conv) do
-    # TODO create a new map that also has the response body
-    conv = %{ method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers" }
+  def route(%{ method: "GET", path: "/wildthings" } = conv) do
+    %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
+
+  def route(%{ method: "GET", path: "/bears" } = conv) do
+    %{ conv | status: 200, resp_body: "Teddy, Smokey, Paddington" }
+  end
+
+  def route(%{ method: "GET", path: "/bears/" <> id } = conv) do
+    %{ conv | status: 200, resp_body: "Bear #{id}" }
+  end
+
+  def route(%{ method: "DELETE", path: "/bears/" <> _id} = conv) do
+    %{ conv | status: 403, resp_body: "Bears must never be deleted!" }
+  end
+
+  def route(%{ path: path } = conv) do
+    %{ conv | status: 404, resp_body: "No #{path} found!" }
+  end
+
+  def emojify(%{ status: 200, resp_body: resp_body } = conv) do
+    %{ conv | resp_body: add_emojis(resp_body) }
+  end
+
+  def emojify(conv), do: conv
+
+  def add_emojis(resp_body), do: ":) #{resp_body} :)"
 
   def format_response(conv) do
-    # TODO: use values in map to generate an HTTP response string
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
+  end
+
+  defp status_reason(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+     }[code]
   end
 end
 
 request = """
 GET /wildthings HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /bears/1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+GET /wildlife HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+# exercises
+
+request = """
+GET /bears?id=1 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+
+IO.puts response
+
+request = """
+DELETE /bears/1 HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
