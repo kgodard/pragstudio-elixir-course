35b7e8d22754e45640c46691a3d36b909fe0e4e0
Kim Godard <kim.godard@gmail.com>
2019-01-12 18:19:57 +0000

    thru ch. 12

---
+++ servy/lib/servy/plugins.ex
@@ -0,0 +1,41 @@
+defmodule Servy.Plugins do
+
+  @moduledoc "Plugins for Servy HTTP server"
+
+  require Logger
+
+  alias Servy.Conv
+
+  def track(%Conv{status: 404, path: path} = conv) do
+    Logger.warn "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(%Conv{} = conv), do: conv
+
+  def rewrite_path(%Conv{path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  def rewrite_path(%Conv{path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path_captures(%Conv{} = conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+
+  def rewrite_path_captures(%Conv{} = conv, nil), do: conv
+
+  def log(%Conv{} = conv), do: IO.inspect conv
+
+  def emojify(%Conv{ status: 200, resp_body: resp_body } = conv) do
+    %{ conv | resp_body: add_emojis(resp_body) }
+  end
+
+  def emojify(%Conv{} = conv), do: conv
+
+  def add_emojis(resp_body), do: ":) #{resp_body} :)"
+end
