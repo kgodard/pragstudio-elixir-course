68912a80d75166752059c9255dba760fa3581d31
Kim Godard <kim.godard@gmail.com>
2019-01-30 00:37:06 +0000

    Ch 26

--- servy/lib/servy/four_oh_four_counter.ex
+++ servy/lib/servy/four_oh_four_counter.ex
@@ -1,84 +1,84 @@
-defmodule Servy.Gserver do
+# defmodule Servy.Gserver do
 
-  def start(callback_module, initial_state, name) do
-    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
-    Process.register(pid, name)
-    pid
-  end
+#   def start(callback_module, initial_state, name) do
+#     pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+#     Process.register(pid, name)
+#     pid
+#   end
 
-  def call(pid, message) do
-    send pid, {:call, self(), message}
+#   def call(pid, message) do
+#     send pid, {:call, self(), message}
 
-    receive do {:response, response} -> response end
-  end
+#     receive do {:response, response} -> response end
+#   end
 
-  def cast(pid, message) do
-    send pid, {:cast, message}
-  end
+#   def cast(pid, message) do
+#     send pid, {:cast, message}
+#   end
 
-  def listen_loop(state, callback_module) do
-    receive do
-      {:call, sender, message} when is_pid(sender) ->
-        {response, new_state} = callback_module.handle_call(message, state)
-        send sender, {:response, response}
-        listen_loop(new_state, callback_module)
-      {:cast, message} ->
-        new_state = callback_module.handle_cast(message, state)
-        listen_loop(new_state, callback_module)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect(unexpected)}"
-        listen_loop(state, callback_module)
-    end
-  end
+#   def listen_loop(state, callback_module) do
+#     receive do
+#       {:call, sender, message} when is_pid(sender) ->
+#         {response, new_state} = callback_module.handle_call(message, state)
+#         send sender, {:response, response}
+#         listen_loop(new_state, callback_module)
+#       {:cast, message} ->
+#         new_state = callback_module.handle_cast(message, state)
+#         listen_loop(new_state, callback_module)
+#       unexpected ->
+#         IO.puts "Unexpected message: #{inspect(unexpected)}"
+#         listen_loop(state, callback_module)
+#     end
+#   end
 
-end
+# end
 
 defmodule Servy.FourOhFourCounter do
 
   @name :fourohfourcounter
 
-  alias Servy.Gserver
+  use GenServer
 
   def start do
     IO.puts "Starting 404 Counter..."
-    Gserver.start(__MODULE__, %{}, @name)
+    GenServer.start(__MODULE__, %{}, name: @name)
   end
 
   def bump_count(path) do
-    Gserver.call @name, {:bump_count, path}
+    GenServer.call @name, {:bump_count, path}
   end
 
   def get_count(path) do
-    Gserver.call @name, {:get_count, path}
+    GenServer.call @name, {:get_count, path}
   end
 
   def get_counts do
-    Gserver.call @name, :get_counts
+    GenServer.call @name, :get_counts
   end
 
   def reset do
-    Gserver.cast @name, :reset
+    GenServer.cast @name, :reset
   end
 
   # Server Callbacks
 
   def handle_cast(:reset, _state) do
-    %{}
+    {:noreply, %{}}
   end
 
-  def handle_call({:bump_count, path}, state) do
+  def handle_call({:bump_count, path}, _from, state) do
     new_state = add_or_increment_key(state, path)
     count = Map.get(new_state, path)
-    {count, new_state}
+    {:reply, count, new_state}
   end
 
-  def handle_call({:get_count, path}, state) do
+  def handle_call({:get_count, path}, _from, state) do
     count = Map.get(state, path)
-    {count, state}
+    {:reply, count, state}
   end
 
-  def handle_call(:get_counts, state) do
-    {state, state}
+  def handle_call(:get_counts, _from, state) do
+    {:reply, state, state}
   end
 
   defp add_or_increment_key(state_map, key) do
--- servy/lib/servy/pledge_controller.ex
+++ servy/lib/servy/pledge_controller.ex
@@ -1,20 +1,29 @@
 defmodule Servy.PledgeController do
   import Servy.View, only: [render: 3]
 
+  alias Servy.PledgeServer
+
   def new(conv) do
     render(conv, "new_pledge.eex", [])
   end
 
   def create(conv, %{"Name" => name, "Amount" => amount}) do
     # sends pledge to external service and caches it
-    Servy.PledgeServer.create_pledge(name, String.to_integer(amount))
+    PledgeServer.create_pledge(name, String.to_integer(amount))
+
+    %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!\n" }
+  end
+
+  def create(conv, %{"name" => name, "amount" => amount}) do
+    # sends pledge to external service and caches it
+    PledgeServer.create_pledge(name, String.to_integer(amount))
 
     %{ conv | status: 201, resp_body: "#{name} pledged #{amount}!\n" }
   end
 
   def index(conv) do
     # Gets the recent pledges from the cache
-    pledges = Servy.PledgeServer.recent_pledges()
+    pledges = PledgeServer.recent_pledges()
 
     render(conv, "recent_pledges.eex", pledges: pledges)
   end
